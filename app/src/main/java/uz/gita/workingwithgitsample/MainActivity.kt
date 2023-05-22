676997d56b5083aa8ebd9bd7501bc05408191904
DilmurodKhudoyberganov <khudouberganovdilmurod636@gmail.com>
2023-05-22 21:39:17 +0500

    qwerty

--- app/src/main/java/uz/gita/workingwithgitsample/MainActivity.kt
+++ app/src/main/java/uz/gita/workingwithgitsample/MainActivity.kt
@@ -3,11 +3,13 @@ package uz.gita.workingwithgitsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("TTT", "onCreate: Hello everyone!")
+        Toast.makeText(this,"dgs",Toast.LENGTH_LONG).show()
     }
 }
