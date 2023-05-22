e5864ee9c92b1a94ba63cfab0d6b8381f89757e5
User <1shavkatovich1@gmail.com>
2023-05-22 21:22:59 +0500

    leg.d

--- app/src/main/java/uz/gita/workingwithgitsample/MainActivity.kt
+++ app/src/main/java/uz/gita/workingwithgitsample/MainActivity.kt
@@ -2,10 +2,12 @@ package uz.gita.workingwithgitsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("TTT", "onCreate: Hello everyone!")
     }
 }
